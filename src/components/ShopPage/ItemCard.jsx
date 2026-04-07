920d1632ad5784e62c7b9f492e4a14aa5d21bebf
Julczan <julek111222h@gmail.com>
2026-04-07 13:35:26 +0200

    test input field, increment and decrement buttons

--- src/components/ShopPage/ItemCard.jsx
+++ src/components/ShopPage/ItemCard.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import { Card } from "./ShopPage-styles";
 
 function ItemCard({ data }) {
+  const [quantity, setQuantity] = useState(data.quantity);
+
+  function handleIncrementClick() {
+    setQuantity(quantity + 1);
+  }
+
+  function handleDecrementClick() {
+    setQuantity(quantity - 1);
+  }
+
   return (
     <Card>
       <img src={data.images[0]} />
       <p>{data.title}</p>
       <p>{data.description}</p>
-      <button>Decrement</button>
-      <input type="number" min={1} max={99} value={data.quantity}></input>
-      <button>Increment</button>
+      <button onClick={handleDecrementClick}>Decrement</button>
+      <input
+        type="number"
+        min={1}
+        max={99}
+        value={quantity}
+        onChange={(e) => setQuantity(+e.target.value)}
+      ></input>
+      <button onClick={handleIncrementClick}>Increment</button>
       <button>Add to cart</button>
     </Card>
   );
--- tests/ShopPage.test.jsx
+++ tests/ShopPage.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import ShopPage from "../src/components/ShopPage/ShopPage";
+import userEvent from "@testing-library/user-event";
 
 window.fetch = vi.fn(() => {
   const shopData = [
@@ -54,7 +55,7 @@ describe("Shop Page", () => {
 });
 
 describe("Shop Item", () => {
-  it("Displays card elements with increment and decrement buttons", async () => {
+  it("Displays card elements with increment, decrement buttons and value in input", async () => {
     render(<ShopPage />);
     const incrementBtns = await screen.findAllByRole("button", {
       name: /increment/i,
@@ -74,15 +75,39 @@ describe("Shop Item", () => {
     expect(decrementBtns).toHaveLength(2);
     expect(images).toHaveLength(2);
     expect(addToCartBtns).toHaveLength(2);
+    expect(inputNumbers[0]).toHaveValue(1);
   });
 
-  // it("Increments and decrements the value in input", async () => {
-  //   const incrementBtn = await screen.findByRole("button", {
-  //     name: /increment/i,
-  //   });
-  //   const decrementBtn = await screen.findByRole("button", {
-  //     name: /decrement/i,
-  //   });
-  //   const inputNumber = await screen.findByRole("spinbutton");
-  // });
+  it("Input value changes if number is provided", async () => {
+    const user = userEvent.setup();
+
+    render(<ShopPage />);
+
+    const inputNumbers = await screen.findAllByRole("spinbutton");
+
+    await user.type(inputNumbers[0], "{backspace}23");
+    await user.type(inputNumbers[1], "{backspace}hello");
+    await user.type(inputNumbers[1], "{backspace}hello");
+
+    expect(inputNumbers[0]).toHaveValue(23);
+    expect(inputNumbers[1]).toHaveValue(0);
+  });
+
+  it("Input value changes on increment/decrement button click", async () => {
+    const user = userEvent.setup();
+
+    render(<ShopPage />);
+
+    const inputNumbers = await screen.findAllByRole("spinbutton");
+    const incrementBtns = await screen.findAllByRole("button", {
+      name: /increment/i,
+    });
+
+    await user.click(incrementBtns[0]);
+    await user.type(inputNumbers[1], "{backspace}23");
+    await user.click(incrementBtns[1]);
+
+    expect(inputNumbers[0]).toHaveValue(2);
+    expect(inputNumbers[1]).toHaveValue(24);
+  });
 });
